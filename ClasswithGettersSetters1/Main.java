6ff190fb2770c3692db80fb031fb9e466698a53a
Yamini <yaminisharathkumar@gmail.com>
2023-07-06 15:24:41 +0530

    JavaProCommit

---
+++ ClasswithGettersSetters1/Main.java
@@ -0,0 +1,13 @@
+package ClasswithGettersSetters1;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleCalculator calc = new SimpleCalculator();
+        calc.setFirstNumber(5.0);
+        calc.setSecondNumber(0);
+        System.out.println("Addition = " + calc.getAdditionResult());
+        System.out.println("Subtraction = " + calc.getSubtractionResult());
+        System.out.println("Multiplication = " + calc.getMultiplicationResult());
+        System.out.println("Division = " + calc.getDivisionResult());
+    }
+}
